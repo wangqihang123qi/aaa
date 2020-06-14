db451cd2d446acc8b889c12c7e52808f25bf0a7b
zhangsan <927182470@qq.com>
2020-06-14 13:12:22 +0800

    第一次发布Git

---
+++ aaa/src/main/java/com/aiguigu/java/taobao.java
@@ -0,0 +1,14 @@
+package com.aiguigu.java;
+
+public class taobao {
+    public static void main(String[] args) {
+        System.out.println("张三");
+        System.out.println("李四");
+        System.out.println("王五");
+        System.out.println("陈");
+        System.out.println("11");
+        System.out.println("22");
+        System.out.println("33");
+
+    }
+}
